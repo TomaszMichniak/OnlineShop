9ca8a1d4ab58106466cfc9fe1da681f4da33c0fb
Misiek01 <Michuu1323@gmail.com>
2023-06-06 12:16:50 +0200

    fix

---
+++ OnlineShop.Application/Image/Command/Delete/DeleteImageCommand.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Application.Image.Command.Delete
+{
+    public class DeleteImageCommand:IRequest
+    {
+        public DeleteImageCommand(string fileName)
+        {
+            FileName = fileName;
+        }
+
+        public string FileName { get; set; }=default!;
+    }
+}
---
+++ OnlineShop.Application/Image/Command/Delete/DeleteImageCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+
+namespace OnlineShop.Application.Image.Command.Delete
+{
+    public class DeleteImageCommandHandler : IRequestHandler<DeleteImageCommand>
+    {
+        private readonly IWebHostEnvironment _hostEnvironment;
+
+        public DeleteImageCommandHandler(IWebHostEnvironment hostEnvironment)
+        {
+            _hostEnvironment = hostEnvironment;
+        }
+
+        public Task<Unit> Handle(DeleteImageCommand request, CancellationToken cancellationToken)
+        { 
+            var filePath = Path.Combine(_hostEnvironment.WebRootPath, "Img", request.FileName);
+            File.Delete(filePath);
+            return Unit.Task;
+        }
+    }
+}
